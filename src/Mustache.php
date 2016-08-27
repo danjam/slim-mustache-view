196ca9548aa63b3c971bf5654ebeb95eddc675b5
danjam <danny@dannyjames.net>
2016-08-27 10:17:53 +0100

    Cleanup and implement getRawTemplate()

--- src/Mustache.php
+++ src/Mustache.php
@@ -11,12 +11,15 @@ use Psr\Http\Message\ResponseInterface;
 class Mustache
 {
     /**
+     * Holds the Mustache_Engine instance
      * @var \Mustache_Engine
      */
     protected $mustache;
 
     /**
-     * @param array $options
+     * Constructor
+     *
+     * @param array $options An array of Mustache.php options
      */
     public function __construct(array $options = [])
     {
@@ -24,9 +27,11 @@ class Mustache
     }
 
     /**
-     * @param $template
-     * @param array $data
-     * @return string
+     * Fetch a rendered template
+     *
+     * @param mixed $template The mustache template string/path
+     * @param array $data The template data
+     * @return string The rendered template
      */
     public function fetch($template, $data = [])
     {
@@ -34,14 +39,27 @@ class Mustache
     }
 
     /**
-     * @param ResponseInterface $response
-     * @param mixed $template
-     * @param array $data
-     * @return ResponseInterface
+     * Render a template to a PSR-7 response object
+     *
+     * @param ResponseInterface $response The PSR-7 response
+     * @param mixed $template The mustache template string/path
+     * @param array $data The template data
+     * @return ResponseInterface A PSR-7 response populated with the rendered mustache template
      */
-    public function render(ResponseInterface $response, $template, array $data = [])
+    public function render(ResponseInterface $response, $template, $data = [])
     {
         $response->getBody()->write($this->fetch($template, $data));
         return $response;
+    }
+
+    /**
+     * Fetch the raw template
+     *
+     * @param mixed $template The mustache template string/path
+     * @return string The raw template contents
+     */
+    public function getRawTemplate($template)
+    {
+        return $this->mustache->getLoader()->load($template);
     }
 }
